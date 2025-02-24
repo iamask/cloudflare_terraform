2b188346072dd7047c8b55a1d0fd5323debb5a2c
Ajay <iamask@live.com>
2025-02-24 12:51:56 +0530

    update main.tf - settings

--- main.tf
+++ main.tf
@@ -24,6 +24,20 @@ provider "cloudflare" {
 }
 
 
+resource "cloudflare_zone_setting" "tf_zone_setting" {
+  zone_id = var.ZONE_ID
+  setting_id = "always_online"
+  id = "0rtt"
+  value = "on"
+}
+
+resource "cloudflare_hostname_tls_setting" "tf_zone_tls_setting" {
+  zone_id = var.ZONE_ID
+  setting_id = "ciphers"
+  hostname = "app.example.com"
+  value = ["ECDHE-RSA-AES128-GCM-SHA256", "AES128-GCM-SHA256"]
+}
+
 
 module "dns" {
   source    = "./dns"
@@ -31,3 +45,40 @@ module "dns" {
   ZONE_ID   = var.ZONE_ID
 }
 
+
+module "managed_rules" {
+  source    = "./security/managed_rules"
+  API_TOKEN = var.API_TOKEN
+  ZONE_ID   = var.ZONE_ID
+}
+
+module "rate_limit_rules" {
+  source    = "./security/rate_limit"
+  API_TOKEN = var.API_TOKEN
+  ZONE_ID   = var.ZONE_ID
+}
+
+module "custom_rules" {
+  source    = "./security/custom_rules"
+  API_TOKEN = var.API_TOKEN
+  ZONE_ID   = var.ZONE_ID
+}
+
+module "cache_rules" {
+  source    = "./rules/cache_rules"
+  API_TOKEN = var.API_TOKEN
+  ZONE_ID   = var.ZONE_ID
+}
+
+
+module "transform_rules" {
+  source    = "./rules/transform_rules"
+  API_TOKEN = var.API_TOKEN
+  ZONE_ID   = var.ZONE_ID
+}
+
+module "redirect_rules" {
+  source    = "./rules/redirect_rules"
+  API_TOKEN = var.API_TOKEN
+  ZONE_ID   = var.ZONE_ID
+}
