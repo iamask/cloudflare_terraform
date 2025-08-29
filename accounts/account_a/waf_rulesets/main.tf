712be124870ce40acd664267b392970829f25051
Ajay <iamask@live.com>
2025-08-29 12:51:27 +0530

    3

--- accounts/account_a/waf_rulesets/main.tf
+++ accounts/account_a/waf_rulesets/main.tf
@@ -1,9 +1,6 @@
 # Account-level WAF Ruleset Configuration
-# Creates custom ruleset at account level
-# 
-# NOTE: To deploy this ruleset, you need to update the existing root ruleset (ID: 1c4cd4c02f38487291480b7824fca8e9)
-# Add an execute rule pointing to this custom ruleset ID after it's created
 
+# Step 1: Create custom ruleset at account level
 resource "cloudflare_ruleset" "account_waf_custom" {
   account_id  = var.ACCOUNT_ID
   name        = "Custom WAF Rules for test.zxc.co.in"
@@ -26,8 +23,37 @@ resource "cloudflare_ruleset" "account_waf_custom" {
   }]
 }
 
-# Output the ruleset ID for reference
+# Step 2: Import and manage existing root ruleset
+# IMPORTANT: First run this import command to take over management of the existing root ruleset:
+# terraform import module.account_waf.cloudflare_ruleset.account_waf_entrypoint account/174f936387e2cf4c433752dc46ba6bb1/1c4cd4c02f38487291480b7824fca8e9
+#
+# After import, this will update the existing root ruleset to execute our custom ruleset
+resource "cloudflare_ruleset" "account_waf_entrypoint" {
+  account_id  = var.ACCOUNT_ID
+  name        = "root"
+  description = ""
+  kind        = "root"
+  phase       = "http_request_firewall_custom"
+
+  # Add our custom ruleset execution rule
+  rules = [{
+    ref         = "deploy_custom_ruleset_zxc"
+    description = "Execute custom ruleset for zxc.co.in"
+    expression  = "(cf.zone.name contains \"zxc.co.in\") and (cf.zone.plan eq \"ENT\")"
+    action      = "execute"
+    action_parameters = {
+      id = cloudflare_ruleset.account_waf_custom.id
+    }
+  }]
+}
+
+# Output the ruleset IDs for reference
 output "custom_ruleset_id" {
   value       = cloudflare_ruleset.account_waf_custom.id
-  description = "ID of the custom WAF ruleset - Deploy by updating existing root ruleset"
+  description = "ID of the custom WAF ruleset"
+}
+
+output "entrypoint_ruleset_id" {
+  value       = cloudflare_ruleset.account_waf_entrypoint.id
+  description = "ID of the root ruleset that deploys the custom ruleset"
 }
