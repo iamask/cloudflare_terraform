8c2e78cba2814ed8f065dea7e581b33cd8eff540
Ajay <iamask@live.com>
2025-08-29 13:06:50 +0530

    GPT

--- accounts/account_a/waf_rulesets/main.tf
+++ accounts/account_a/waf_rulesets/main.tf
@@ -1,53 +1,43 @@
-# Account-level WAF Ruleset Configuration
-
-# Step 1: Create custom ruleset at account level
 resource "cloudflare_ruleset" "account_waf_custom" {
   account_id  = var.ACCOUNT_ID
-  name        = "Custom WAF Rules for test.zxc.co.in"
-  description = "Account-level custom firewall ruleset - Geo blocking"
+  name        = "Custom ruleset blocking traffic in non-standard HTTP(S) ports"
+  description = ""
   kind        = "custom"
   phase       = "http_request_firewall_custom"
 
   rules = [{
-    ref         = "block_us_traffic"
-    description = "Block US traffic to test.zxc.co.in"
-    expression  = "(http.host eq \"test.zxc.co.in\" and ip.geoip.country eq \"US\")"
+    ref         = "block_non_default_ports"
+    description = "Block ports other than 80 and 443"
+    expression  = "(not cf.edge.server_port in {80 443})"
     action      = "block"
-    action_parameters = {
-      response = {
-        status_code  = 403
-        content      = "Access denied - Geographic restriction"
-        content_type = "text/plain"
-      }
-    }
   }]
 }
 
-# Step 2: Import and manage existing root ruleset
-# IMPORTANT: First run this import command to take over management of the existing root ruleset:
-# terraform import module.account_waf.cloudflare_ruleset.account_waf_entrypoint account/174f936387e2cf4c433752dc46ba6bb1/1c4cd4c02f38487291480b7824fca8e9
-#
-# After import, this will update the existing root ruleset to execute our custom ruleset
 resource "cloudflare_ruleset" "account_waf_entrypoint" {
   account_id  = var.ACCOUNT_ID
   name        = "root"
   description = ""
   kind        = "root"
   phase       = "http_request_firewall_custom"
+  depends_on  = [cloudflare_ruleset.account_waf_custom]
 
   # Add our custom ruleset execution rule
   rules = [{
     ref         = "deploy_custom_ruleset_zxc"
-    description = "Execute custom ruleset for zxc.co.in"
-    expression  = "(cf.zone.name contains \"zxc.co.in\") and (cf.zone.plan eq \"ENT\")"
+    description = "Deploy custom ruleset for example.com"
+    expression  = "(cf.zone.name eq \"example.com\") and (cf.zone.plan eq \"ENT\")"
     action      = "execute"
     action_parameters = {
       id = cloudflare_ruleset.account_waf_custom.id
     }
   }]
+  
+  lifecycle {
+    # Prevent accidental recreation - we're managing an existing resource
+    prevent_destroy = true
+  }
 }
 
-# Output the ruleset IDs for reference
 output "custom_ruleset_id" {
   value       = cloudflare_ruleset.account_waf_custom.id
   description = "ID of the custom WAF ruleset"
