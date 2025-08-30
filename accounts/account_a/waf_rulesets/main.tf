4691a408005a19d591631d0223c0eafe17695992
Ajay <iamask@live.com>
2025-08-30 21:09:39 +0530

    this-5

--- accounts/account_a/waf_rulesets/main.tf
+++ accounts/account_a/waf_rulesets/main.tf
@@ -1,54 +1,115 @@
-# Step 1: Create/update the custom ruleset with your rules
-resource "cloudflare_ruleset" "account_waf_custom" {
+################################################################################
+# Account-Level Custom WAF Rules Configuration
+# This module creates and deploys custom WAF rules at the account level
+# using Cloudflare Terraform Provider v5
+################################################################################
+
+# Step 1: Create the custom ruleset with your WAF rules
+resource "cloudflare_ruleset" "account_custom_rules" {
   account_id  = var.ACCOUNT_ID
-  name        = "Custom ruleset blocking traffic in non-standard HTTP(S) ports"
-  description = "Custom WAF rules for account"
+  name        = "Account Custom WAF Rules"
+  description = "Custom WAF ruleset with security rules for the account"
   kind        = "custom"
   phase       = "http_request_firewall_custom"
 
-  rules = [{
-    description = "Block ports other than 80 and 443"
-    expression  = "(not cf.edge.server_port in {80 443})"
-    action      = "block"
-    enabled     = true
-  }]
+  # Define your custom rules here
+  rules = [
+    {
+      action      = "block"
+      description = "Block non-standard ports (other than 80 and 443)"
+      enabled     = true
+      expression  = "(not cf.edge.server_port in {80 443})"
+    },
+    {
+      action      = "block"
+      description = "Block US traffic to www.zxc.co.in"
+      enabled     = true
+      expression  = "(http.host eq \"www.zxc.co.in\" and ip.geoip.country eq \"US\")"
+    },
+    {
+      action = "challenge"
+      action_parameters = {
+        version = "managed"
+      }
+      description = "Challenge suspicious User-Agents"
+      enabled     = true
+      expression  = "(http.user_agent contains \"bot\" and not cf.bot_management.verified_bot)"
+    },
+    {
+      action = "log"
+      action_parameters = {
+        request = {
+          fields = ["cf.threat_score", "cf.edge.server_port", "http.request.uri.path"]
+        }
+      }
+      description = "Log high threat score requests"
+      enabled     = true
+      expression  = "(cf.threat_score > 30)"
+    }
+  ]
 }
 
-# Step 2: Use PUT to update the existing root ruleset
-# NOTE: This will replace the existing root ruleset with ID: 1c4cd4c02f38487291480b7824fca8e9
-# Any existing rules in that ruleset will be replaced
-resource "cloudflare_ruleset" "account_waf_entrypoint" {
+# Step 2: Deploy the custom ruleset by updating the account-level root ruleset
+resource "cloudflare_ruleset" "account_deploy_custom_rules" {
   account_id  = var.ACCOUNT_ID
-  name        = "root"
-  description = "Account-level WAF Custom Phase"
+  name        = "Account WAF Deployment"
+  description = "Deploys custom WAF rules at account level"
   kind        = "root"
   phase       = "http_request_firewall_custom"
-  
-  # Deploy our custom ruleset for specific zones
-  rules = [{
-    description = "Deploy custom ruleset for zxc.co.in zones"
-    expression  = "(cf.zone.name contains \"zxc.co.in\")"
-    action      = "execute"
-    action_parameters = {
-      id = cloudflare_ruleset.account_waf_custom.id
-    }
-    enabled = true
-  }]
 
-  depends_on = [cloudflare_ruleset.account_waf_custom]
-  
-  lifecycle {
-    # This will replace the existing root ruleset
-    create_before_destroy = false
-  }
+  # Rule to execute the custom ruleset for specific zones or all zones
+  rules = [
+    {
+      action = "execute"
+      action_parameters = {
+        id = cloudflare_ruleset.account_custom_rules.id
+      }
+      description = "Execute custom rules for zxc.co.in zones"
+      enabled     = true
+      expression  = "(cf.zone.name contains \"zxc.co.in\")"
+    }
+    # You can add more deployment rules for different zones or conditions
+    # Example: Deploy to all zones
+    # {
+    #   action = "execute"
+    #   action_parameters = {
+    #     id = cloudflare_ruleset.account_custom_rules.id
+    #   }
+    #   description = "Execute custom rules for all zones"
+    #   enabled     = true
+    #   expression  = "true"
+    # }
+  ]
 }
 
+################################################################################
+# Outputs
+################################################################################
+
 output "custom_ruleset_id" {
-  value       = cloudflare_ruleset.account_waf_custom.id
+  value       = cloudflare_ruleset.account_custom_rules.id
   description = "ID of the custom WAF ruleset"
 }
 
-output "root_ruleset_id" {
-  value       = cloudflare_ruleset.account_waf_entrypoint.id
-  description = "ID of the root ruleset"
+output "deployment_ruleset_id" {
+  value       = cloudflare_ruleset.account_deploy_custom_rules.id
+  description = "ID of the deployment ruleset"
+}
+
+output "deployment_status" {
+  value = <<-EOT
+    Custom WAF Rules Deployed Successfully!
+    
+    Custom Ruleset ID: ${cloudflare_ruleset.account_custom_rules.id}
+    Deployment ID: ${cloudflare_ruleset.account_deploy_custom_rules.id}
+    
+    The following rules are now active:
+    - Block non-standard ports (other than 80 and 443)
+    - Block US traffic to www.zxc.co.in
+    - Challenge suspicious User-Agents
+    - Log high threat score requests
+    
+    Applied to: Zones containing "zxc.co.in"
+  EOT
+  description = "Deployment status and summary"
 }
